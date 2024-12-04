00d613a4c08c47c1b3596b62d51626654124d496
Esubale Endale <esu1011esu@gmail.com>
2024-12-04 23:42:52 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-700 font-bold">Vite + React</h1>
+
+      <div className="bg-blue-500 text-white p-4 rounded">
+        Hello, TailwindCSS with Vite!
+      </div>
+    </>
+  );
+}
+
+export default App;
